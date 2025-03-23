7489e50cc41461fca42f7f5e783ca203aa28ba8f
b1ron <goldingbyron@gmail.com>
2025-03-23 06:28:13 +0100

    add Magdir and files

--- mime.js
+++ mime.js
@@ -1,21 +1,52 @@
-// MIME is a registry of the standard MIME types and custom file types.
+// MIME type detection
 
 const fs = require('node:fs');
 
-// cont    offset  type    opcode  mask    value   desc
-// metrics.2021-03-15T02-21-47Z-00000
 try {
-  const data = fs.readFileSync('metrics.2021-03-15T02-21-47Z-00000');
-  console.log(strings(data));
+  let data = fs.readFileSync('files/small.pdf');
+  console.log(dump(data));
+  data = fs.readFileSync('files/metrics.2021-03-15T02-21-47Z-00000');
+  console.log(dump(data));
+// { sp: '', hex: '25 50 44 46 2d 31 2e 34', ascii: '%PDF-1.4' }
+// { sp: '', hex: 'e5 2f 00 00 09 5f 69 64', ascii: 'å/\x00\x00\t_id' }
 } catch (err) {
   console.error(err);
 }
 
+// { sp: '', hex: '25 50 44 46 2d 31 2e 34', ascii: '%PDF-1.4' } - PDF file with magic pattern string at offset 0 and version at offset 5
+// 0	string		%PDF-		PDF document
+// !:mime	application/pdf
+// !:strength +60
+// !:ext	pdf
+// >5	byte		x		\b, version %c
+// >7	byte		x		\b.%c
+// >0	use		pdf
+
+// An xxd-like function to dump the first 8 bytes of a buffer in hex and ASCII.
+function dump(buffer, maxLength = 8) {
+  let output = {
+    sp: ' ',
+    hex: '',
+    ascii: '',
+  };
+  for (let i = 0; i < maxLength; i++) {
+    const hex = parseInt(buffer[i], 10).toString(16);
+    if (i >= maxLength-1) {
+      output.sp = '';
+    }
+    output.hex += hex.padStart(2, '0') + output.sp;
+    output.ascii += String.fromCharCode(buffer[i]);
+  }
+
+  return output;
+}
+
+// A re-implementation of the Unix strings command.
 function strings(buffer, minLength = 4) {
   const printableChars = /^[\x20-\x7E]+$/; // ASCII printable characters
   let result = [];
   let currentString = '';
-
+  
   for (let i = 0; i < buffer.length; i++) {
     const char = String.fromCharCode(buffer[i]);
     if (printableChars.test(char)) {
@@ -27,11 +58,10 @@ function strings(buffer, minLength = 4) {
       currentString = '';
     }
   }
-
+  
   if (currentString.length >= minLength) {
     result.push(currentString);
   }
-
+  
   return result.join('\n');
 }
-
