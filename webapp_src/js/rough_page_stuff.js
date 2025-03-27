a4216005586943763b63e6dfdb7a166f9a50560c
Akira Kurogane <akira.kurogane@gmail.com>
2025-03-27 14:15:50 +0900

    Filling list of sample files from S3 bucket automatically on page load

--- webapp_src/js/rough_page_stuff.js
+++ webapp_src/js/rough_page_stuff.js
@@ -1,23 +1,66 @@
 import { readFTDCFile } from './ftdc_parser.js';
 
-let addFetchItBtn;
 let debugPaneDiv;
 
-const fetchFtdcAndDisplay = async () => {
-    const hostPrefix = "https://lfs-example-files-202503.s3.ap-northeast-1.amazonaws.com/";
-    const fpath = "wt_internals_training/diagnostic.data/metrics.2021-03-11T08-20-52Z-00000";
-    readFTDCFile(`${hostPrefix}${fpath}`);
-    //let x = await fetch(`${hostPrefix}${fpath}`);
-    //let y = await x.text();
-    //console.log(y);
-    debugPaneDiv.innerText = "adfsfasdfa";
+const updateDebugPaneHTML = (newHTML) => {
+    debugPaneDiv.innerHTML = newHTML;
+};
+const appendDebugPaneHTML = (newHTML) => {
+    const h = debugPaneDiv.innerHTML;
+    debugPaneDiv.innerHTML = h + (h ? "<br/>" : "") + newHTML;
+};
+
+const s3BucketName = "lfs-example-files-202503";
+const s3BucketRegion = "ap-northeast-1";
+const s3BucketUrl = `https://${s3BucketName}.s3.${s3BucketRegion}.amazonaws.com/`;
+
+/**
+ * Using https://docs.aws.amazon.com/AmazonS3/latest/API/API_ListObjectsV2.html to list files in
+ *   the bucket. Note this endpoint only works when the "s3:ListBucket" action is granted by bucket
+ *   policy on the bucket.
+ * AWS's underlying HTTP API is XML, so data is parsed and iterated here by ancient ways.
+ */
+const listBucketObjects = async (bucketUrl) => {
+    const res = [];
+    const fr = await fetch(`${bucketUrl}?list-type-2`);
+    const tr = await fr.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(tr, "text/xml");
+    const listBRElem = doc.documentElement; //The top-most element, <ListBucketResult>
+    const objElems = listBRElem.getElementsByTagName("Contents");
+    for (let i = 0; i < objElems.length; i++) {
+        let o = objElems.item(i);
+        res.push(o.getElementsByTagName("Key")[0].textContent);
+    }
+    return res;
+};
+
+const fetchFtdcAndDisplay = async (fpath) => {
+    readFTDCFile(`${s3BucketUrl}${fpath}`);
+    appendDebugPaneHTML(fpath + "<br/>" + "See console log output");
 };
 
 const dynamicContentSetup = async () => {
-    addFetchItBtn = document.createElement("button");
-    addFetchItBtn.innerText = "Fetch it";
-    document.body.append(addFetchItBtn);
-    addFetchItBtn.addEventListener('click', fetchFtdcAndDisplay);
+
+    const filesUlElem = document.createElement("ul");
+    filesUlElem.id = "bucket_files_list";
+    filesUlElem.innerHTML = `S3 bucket <strong>${s3BucketName}</strong> (${s3BucketRegion})`;
+    document.body.append(filesUlElem);
+
+    const bucketFilesList = await listBucketObjects(s3BucketUrl);
+    if (!bucketFilesList || bucketFilesList.length === 0) {
+        const errLiElem = document.createElement("li");
+        errLiElem.innerText = "Ara! No files found in the bucket.";
+        filesUlElem.append(errLiElem);
+    }
+    for (let i = 0; i < bucketFilesList.length; i++) {
+        const liElem = document.createElement("li");
+        liElem.innerHTML = `${bucketFilesList[i]} <button>Fetch it</button>`;
+        filesUlElem.append(liElem);
+        liElem.querySelector("button").addEventListener('click', async () => {
+            fetchFtdcAndDisplay(bucketFilesList[i])
+        });
+    }
 
     debugPaneDiv = document.createElement("div");
     debugPaneDiv.id = "debug_pane";
